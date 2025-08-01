a9510ba621264217756a61478b19d3baa8963c61
Don Miranda <aldrinpm@gmail.com>
2025-08-01 08:34:56 +0800

    update firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -6,13 +6,15 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBJxkHBr9F7XHdd0eNYUgV1DAojDXLA_lc",
-  authDomain: "react-js-todo-c1c9c.firebaseapp.com",
-  projectId: "react-js-todo-c1c9c",
-  storageBucket: "react-js-todo-c1c9c.firebasestorage.app",
-  messagingSenderId: "963388911923",
-  appId: "1:963388911923:web:59de91d91117b0fe50ce46"
+  apiKey: "AIzaSyCMjanJ-uww7YPHUgbenQlsjgYT7H8MLXc",
+  authDomain: "react-js-todo-8f859.firebaseapp.com",
+  projectId: "react-js-todo-8f859",
+  storageBucket: "react-js-todo-8f859.firebasestorage.app",
+  messagingSenderId: "1050234510914",
+  appId: "1:1050234510914:web:e42ec09ffcd6e80bedd7db",
+  measurementId: "G-N9445CFDHK"
 };
 
 // Initialize Firebase
